7feaace0df38937627af181b3ff70a7d9b0f6175
ManuelAUT <51486213+ManuelAUT@users.noreply.github.com>
2021-06-14 17:59:23 +0200

    yaml change

--- buy.php
+++ buy.php
@@ -1,55 +1,18 @@
-<?php
+use Google\Cloud\PubSub\PubSubClient;
 
-require_once 'vendor/autoload.php';
-
-use WindowsAzure\Common\ServicesBuilder;
-use WindowsAzure\Common\ServiceException;
-use WindowsAzure\ServiceBus\Models\QueueInfo;
-use WindowsAzure\ServiceBus\Models\BrokeredMessage;
-
-if ($_POST['menge'] < 0 ) {
-  echo "<script>alert('Falscher Input')</script>";
-  header("Location: ./index.php");
-} else {
-
-try {
-   
-	$id = $_POST['artnr'];
-	$menge = $_POST['menge'];
-	$comment = $_POST['comment'];
-
+/**
+ * Publishes a message for a Pub/Sub topic.
+ *
+ * @param string $projectId  The Google project ID.
+ * @param string $topicName  The Pub/Sub topic name.
+ * @param string $message  The message to publish.
+ */
+function publish_message($projectId, $topicName, $message)
+{
+    $pubsub = new PubSubClient([
+        'projectId' => $projectId,
+    ]);
+    $topic = $pubsub->topic($topicName);
+    $topic->publish(['data' => $message]);
+    print('Message published' . PHP_EOL);
 }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage() . "\n";
-    }
-
-    $connectionString = "Endpoint=sb://direktsms.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=CXz81wQJmzr0DxtBDnXtWrFGiYNjo61axinpu0B8bFQ=";
-    // Create Service Bus REST proxy.
-     $serviceBusRestProxy = ServicesBuilder::getInstance()->createServiceBusService($connectionString);
-
-try    {
-     echo "create msg";
-     // Create message.
-     $message = new BrokeredMessage();
-     $message->setBody("message");
-     echo "send msg";
-     echo "ArtNr: " . "$id" . ", Menge: " . "$menge" . ", Comment :" . "$comment";
-     // Send message.
-     $serviceBusRestProxy->sendQueueMessage("ringe", $message);
-     echo "SMS";
- }
- catch(ServiceException $e){
-     // Handle exception based on error codes and messages.
-     // Error codes and messages are here: 
-     // https://docs.microsoft.com/rest/api/storageservices/Common-REST-API-Error-Codes
-     $code = $e->getCode();
-     $error_message = $e->getMessage();
-     echo $code.": ".$error_message."<br />";
- }
-
-}
-?>
-<br>
-<br>
-<a href=./>Back to Main</a>
